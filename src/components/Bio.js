097c4fbd31e7d7c4943bd4d1b8e40d260b062b29
charles arnaudo <charlesarnaudo@gmail.com>
2019-01-09 10:30:08 -0500

    some updates

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-import Image from 'gatsby-image'
 
 import { rhythm } from '../utils/typography'
 
@@ -9,7 +8,7 @@ function Bio() {
     <StaticQuery
       query={bioQuery}
       render={data => {
-        const { author, social } = data.site.siteMetadata
+        const { author } = data.site.siteMetadata
         return (
           <div
             style={{
