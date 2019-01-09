7ae068a233870c8443c4728313ac4d3e3081e1a6
charles arnaudo <charlesarnaudo@gmail.com>
2019-01-08 20:24:07 -0500

    first post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `My Blog`,
+    author: `Charles Arnaudo`,
+    description: `Whatever I want it to be`,
+    siteUrl: `https://charlesarnaudo.github.io/`,
   },
   plugins: [
     {
@@ -57,13 +54,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `My Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
+        background_color: `#3c3c3c`,
+        theme_color: `#3c4c55`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
