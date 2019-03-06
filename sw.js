d62b2a76732067082f3fed3438dbc4fed5d15ac8
charles <charlesarnaudo@gmail.com>
2019-03-06 10:40:14 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d0371eecc4e8391880c5.js"
+    "url": "webpack-runtime-63de5544a21ff38e4a38.js"
   },
   {
     "url": "app.bddaef654c66ded1434d.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f575c7544ab7e6a1f76be0ae43c30817"
+    "revision": "c433d2dafe09a9331caa94351cb95780"
   },
   {
     "url": "0-1237e3845cace67130aa.js"
